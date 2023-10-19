2c05f5a81732db0c543d8c071684f8e052467c0b
example <email@example.com>
2023-10-19 12:16:37 +0400

    edit3

--- React_quiz/frontend/src/containers/Edit/EditQuestion/EditQuestion.jsx
+++ React_quiz/frontend/src/containers/Edit/EditQuestion/EditQuestion.jsx
@@ -1,12 +1,53 @@
 import React from 'react'
 import { BsFillTrashFill, BsPlusCircleFill,BsCheckCircle,BsFillCheckCircleFill } from "react-icons/bs";
-
+import classNames from 'classnames';
+import { getId } from '../../../shared/utils/uniqueId';
 
 const initialOptions = {
     content: '',
-    options: []
+    options: [],
+    changed: false,
 }
 
+const changeOptionAction = (state,action) => {
+    const newState = {...state,changed:true}
+    const newOptions = [...newState.options]
+    const newOption = {...newOptions[action.index],answer:action.answer}
+    newOptions[action.index] = newOption
+    newState.options = newOptions
+    return newState
+}
+
+
+const selectCorrectAction = (state,action) => {
+    console.log(action)
+    const newOptions = state.options.map((o,i) => ({...o,correct:action.index === i}))
+    return{...state,changed:true,options: newOptions}
+}
+
+const deleteOptionAction = (state,action) =>{ 
+    // return {...state,changed:true,options: state.options.filter((o,i) => i!== action.index)}
+    const newState = {...state,changed: true}
+    if (newState.options.length < 3){
+        return state
+    }
+    const newOptions = [...newState.options]
+    const deletedArray = newOptions.splice(action.index,1)
+    const deletedOption = deletedArray[0]
+    if(deletedOption.correct){
+        newOptions[0] = {...newOptions[0],correct: true}
+    }
+    newState.options = newOptions
+    return newState
+}
+
+const addOptionAction = (state,action) =>{
+    return {
+        ...state,
+        changed:true,
+        options:[...state.options,{id:getId(),answer: 'New Option',correct:false}]
+    }
+}
 
 
 const reducer = (state,action) => {
@@ -14,7 +55,15 @@ const reducer = (state,action) => {
         case 'LOAD_QUESTION':
             return action.question
         case 'CHANGE_CONTENT':
-            return {...state,content: action.content}
+            return {...state,content: action.content,changed:true}
+        case 'CHANGE_OPTION':
+            return changeOptionAction(state,action)
+        case 'SELECT_CORRECT':
+            return selectCorrectAction(state,action)
+        case 'DELETE_OPTION':
+            return deleteOptionAction(state,action)
+        case 'ADD_OPTION':
+            return addOptionAction(state,action)
         default:
             return state
     }
@@ -22,12 +71,10 @@ const reducer = (state,action) => {
 
 function EditQuestion(props) {
   const[question,dispatch] = React.useReducer(reducer,initialOptions);
-  const[changed,setChanged] = React.useState(false);
   const textAreaRef = React.useRef()
 
     const changeContentHandler = React.useCallback((e) =>{
         dispatch({type:'CHANGE_CONTENT',content: e.target.value})
-        setChanged(true)
     },[])
 
 
@@ -46,15 +93,14 @@ function EditQuestion(props) {
   return (
     <div className='w-full bg-slate-200 p-3 pl-1 rounded relative'>
 
-        <div className='self-center absolute right-[-25px] top-[-20px] text-3xl text-red-600  cursor-pointer p-2'><BsFillTrashFill/></div>
-        {changed && <div className='self-center absolute right-[-28px] bottom-[-12px] text-3xl text-green-700  cursor-pointer p-3'><BsCheckCircle/></div>}
+        <div onClick={props.onDelete} className='self-center absolute right-[-25px] top-[-20px] text-3xl text-red-600  cursor-pointer p-2'><BsFillTrashFill/></div>
+        {question.changed && <div className='self-center absolute right-[-28px] bottom-[-12px] text-3xl text-green-700  cursor-pointer p-3'><BsCheckCircle/></div>}
 
         <div className='flex mb-3'>
             <div className='w-8 text-center text-xl font-bold'>{props.index + 1}</div>
             <textarea 
             type="text" 
             className='resize-none w-full rounded p-3 outline-none' 
-            rows={5}
             value={question.content}
             onChange={changeContentHandler}
             ref={textAreaRef}
@@ -63,17 +109,20 @@ function EditQuestion(props) {
         </div>
         <div>
             {question.options.map((option,index) => {
+                const optionChange = e => dispatch({type:'CHANGE_OPTION',index: index,answer: e.target.value})
+                const onCheckedClick = e => dispatch({type:'SELECT_CORRECT',index: index})
+                const onOptionDelete = e => dispatch({type: 'DELETE_OPTION',index: index})
                 return(
                     <div  key={option.id} className='flex items-center mb-3'>
                         <span className='w-8 text-center'>{String.fromCharCode(index+65)})</span>
-                        <input value={option.answer} type="text" className='w-full rounded p-1 outline-none mr-2'/>
-                        <div className="text-green-700 mr-2  cursor-pointer"><BsFillCheckCircleFill/></div>
-                        <div className="text-red-600 cursor-pointer"><BsFillTrashFill/></div>
+                        <input value={option.answer} onChange={optionChange} type="text" className='w-full rounded p-1 outline-none mr-2'/>
+                        <div onClick={onCheckedClick} className={classNames("mr-2  cursor-pointer",{"text-green-700": option.correct})}><BsFillCheckCircleFill/></div>
+                        <div onClick={onOptionDelete} className="text-red-600 cursor-pointer"><BsFillTrashFill/></div>
 
                     </div>
                 )
             })}
-           <div className="flex justify-center text-3xl text-blue-900 cursor-pointer">
+           <div onClick={() => dispatch({type:'ADD_OPTION'}) }className="flex justify-center text-3xl text-blue-900 cursor-pointer">
                 <BsPlusCircleFill/>
            </div>
         </div>
--- React_quiz/frontend/src/containers/Edit/EditQuiz/EditQuiz.jsx
+++ React_quiz/frontend/src/containers/Edit/EditQuiz/EditQuiz.jsx
@@ -4,30 +4,47 @@ import EditQuestion from '../EditQuestion/EditQuestion'
 
 function EditQuiz(props) {
     const[quiz,setQuiz] = React.useState({});
-
+    const[quizTitle,setQuizTitle] = React.useState('')
 
     React.useEffect(() => {
         getQuestions(props.quizId).then(response =>{
             setQuiz(response.data)
+            setQuizTitle(response.data.title)
         })
     },[props.quizId])
 
+
+
+  const deleteQuestionHandler = React.useCallback((id) =>{
+        setQuiz(prev => ({...prev,questions: prev.questions.filter(q => q.id !== id)}))
+  },[])
+
   return (
     <div>
-        <div className='text-2xl text-center p-2  font-bold'>Edit Quiz</div>
+  
+        <div className="flex p-5 justify-center">
+            <input onChange={e => setQuizTitle(e.target.value)} value={quizTitle} type='text' className='text-xl border text-center font-bold rounded p-1 '/>
+            <div className='w-2/12 bg-blue-900 cursor-pointer text-white p-2 text-center font-bold'>Save</div>
+        </div>
+    
         <div>
             {quiz.questions && quiz.questions.map((question,index) => {
+                const onDelete = () => deleteQuestionHandler(question.id)
                 return(
                     <div key={question.id} className='w-9/12 p-3 m-auto mb-5'>
-                        <EditQuestion question={question} index={index}/>
+                        <EditQuestion question={question} onDelete={onDelete} index={index}/>
                     </div>
                 )
             })}
+            <div className='flex justify-center'>
+                <div className='bg-blue-900 font-bold p-3 text-white rounded cursor-pointer'>Add Question</div>
+            </div>
        
         </div>
       
     </div>
   )
+  
 }
 
 export default EditQuiz
